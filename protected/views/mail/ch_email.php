19988ef8a80b8cfbd1244520bee4238e7b3251ce
Dmitriy Paunin <paunin@immo.ru>
2013-09-23 15:49:42 +0400

    i18n email

--- protected/views/mail/ch_email.php
+++ protected/views/mail/ch_email.php
@@ -1,12 +1,13 @@
 <?php  if (empty($alt) || !$alt)
     require(dirname(__FILE__) . '/_header.php')?>
-    Hello! <br/><br/>
+    <?php echo Yii::t('email','Hello!');?> <br/><br/>
 
-    To complete <b>email change</b> process click this link<?php if(!empty($old_email)): ?>(old email <?php echo  $old_email ?>) <?php endif; ?>:<br/>
-    <a href="<?php echo $url; ?>" style="color: #EE611C"><?php echo $url; ?></a>
+    <?php echo Yii::t('email','To complete <b>email change</b> process click this link');?>
+    <?php if(!empty($old_email)): ?>(<?php echo Yii::t('email','old email');?> <?php echo  $old_email ?>) <?php endif; ?>:<br/>
+    <a href="<?php echo $url; ?>"><?php echo $url; ?></a>
     <br/>
     <br/>
-    Please ignore this message if you not sure in future actions.<br/><br/>
+    <?php echo Yii::t('email','Please ignore this message if you not sure in future actions.');?><br/><br/>
 
 <?php  if (empty($alt) || !$alt)
     require(dirname(__FILE__) . '/_footer.php')?>
--- protected/views/mail/recovery_pass.php
+++ protected/views/mail/recovery_pass.php
@@ -1,12 +1,12 @@
-<?php  if (empty($alt) || !$alt)
+<?php  if(empty($alt) || !$alt)
     require(dirname(__FILE__) . '/_header.php')?>
-    Hello! <br/><br/>
+    <?php echo Yii::t('email', 'Hello!'); ?> <br/><br/>
 
-    To complete <b>password recovery</b> process click this link:<br/>
-    <a href="<?php echo $url; ?>" style="color: #EE611C"><?php echo $url; ?></a>
+    <?php echo Yii::t('email','To complete <b>password recovery</b> process click this link:');?><br/>
+    <a href="<?php echo $url; ?>"><?php echo $url; ?></a>
     <br/>
     <br/>
-    Please ignore this message if you not sure in future actions.<br/><br/>
+    <?php echo Yii::t('email','Please ignore this message if you not sure in future actions.');?><br/><br/>
 
-<?php  if (empty($alt) || !$alt)
+<?php  if(empty($alt) || !$alt)
     require(dirname(__FILE__) . '/_footer.php')?>
