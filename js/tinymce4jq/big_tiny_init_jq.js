ebcda2115ebe97741416c66175eb166a904dd448
paunin <info@paunin.com>
2013-09-24 00:32:11 +0400

    filemanager

--- js/tinymce4jq/big_tiny_init_jq.js
+++ js/tinymce4jq/big_tiny_init_jq.js
@@ -1,19 +1,18 @@
 $(document).ready(function () {
     $('.big_tiny').tinymce({
-
         theme: "modern",
         plugins: [
             "advlist autolink lists link image charmap print preview hr anchor pagebreak",
             "searchreplace wordcount visualblocks visualchars code fullscreen",
             "insertdatetime media nonbreaking save table contextmenu directionality",
-            "emoticons template paste textcolor moxiemanager responsivefilemanager"
+            "emoticons template paste textcolor"
         ],
         toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
-        toolbar2: "responsivefilemanager  print preview code media | forecolor backcolor emoticons",
+        toolbar2: "print preview code media | forecolor backcolor emoticons",
         image_advtab: true ,
-        external_filemanager_path:"/js/responsive_filemanager/filemanager/",
+        external_filemanager_path:"/web/responsive_filemanager/filemanager/",
         filemanager_title:"Responsive Filemanager" ,
-        external_plugins: { "filemanager" : "/js/responsive_filemanager/filemanager/plugin.min.js"}
+        external_plugins: { "filemanager" : "/web/responsive_filemanager/filemanager/plugin.min.js"}
 
 
     });
