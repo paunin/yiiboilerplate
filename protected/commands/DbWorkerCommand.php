a744ab9943b52c94f9a5c3234926f87c376a5d4b
paunin <info@paunin.com>
2013-09-23 20:45:52 +0400

    dbWorkerFix

--- protected/commands/DbWorkerCommand.php
+++ protected/commands/DbWorkerCommand.php
@@ -1,45 +1,62 @@
 <?php
 class DbWorkerCommand extends CConsoleCommand
 {
+    private $pdd, $pdu, $pdp;
 
     /** @var $db CDbConnection */
     private $db = null;
     public function init(){
+        $this->pdd = Yii::app()->params['pgsqlDb'];
+        $this->pdu = Yii::app()->params['pgsqlUser'];
+        $this->pdp = Yii::app()->params['pgsqlPassword'];
         $this->db = Yii::app()->db;
     }
 
     public function actionLoad($path=null){
 
         if(!$path)
-            $path = dirname(__FILE__).'/../tests/fixtures/dbinit.sql';
+            $path = $this->getDefaultFile();
 
-        $pdd = Yii::app()->params['pgsqlDb'];
-        $pdu = Yii::app()->params['pgsqlUser'];
-        $pdp = Yii::app()->params['pgsqlPassword'];
-        $command = "env PGPASSWORD=$pdp  psql";
-
-        if($pdu =='postgres')
+        `{$this->getPreCommand()}`;
+        $command = "psql";
+        if($this->pdu =='postgres')
             die('Your database can\'t be used by postgres');
-        //$this->db->createCommand("DROP OWNED BY $pdu CASCADE")->execute();
-        echo "drop...\n";
 
-        
-        `$command -h localhost -U $pdu -c"DROP OWNED BY $pdu CASCADE;"`;
-        echo "load...\n";
 
-        `$command -h localhost -U $pdu $pdd < $path`;
+        echo "droping...";
+        `$command -h localhost -U {$this->pdu} -c"DROP OWNED BY {$this->pdu} CASCADE;"`;
+        echo "done\n";
+
+        echo "loading...";
+        `$command -h localhost -U {$this->pdu} {$this->pdd} < $path`;
+        echo "done\n";
+
         echo 'ok'."\n";
     }
 
     public function actionSave($path = null){
         if(!$path)
-            $path = dirname(__FILE__).'/../tests/fixtures/dbinit.sql';
-        $pdd = Yii::app()->params['pgsqlDb'];
-        $pdu = Yii::app()->params['pgsqlUser'];
-        $pdp = Yii::app()->params['pgsqlPassword'];
-        $command = "env PGPASSWORD=$pdp pg_dump --encoding=UTF8";
+            $path = $this->getDefaultFile();
 
-        `$command -h localhost -U $pdu -O -x $pdd > $path`;
+        `{$this->getPreCommand()}`;
+        $command = "pg_dump --encoding=UTF8";
+
+        `$command -h localhost -U {$this->pdu} -O -x {$this->pdd} > $path`;
         echo 'ok'."\n";
+    }
+
+    private function getDefaultFile(){
+        return dirname(__FILE__).'/../tests/fixtures/dbinit.sql';
+    }
+
+    private function getPreCommand(){
+        if(empty($this->pdp))
+            return '';
+
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')
+            $command = "set PGPASSWORD={$this->pdp}";
+        else
+            $command = "env PGPASSWORD={$this->pdp}";
+        return $command;
     }
 }
