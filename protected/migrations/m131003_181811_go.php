1f4ad5a591b6ed5732c8b99d08e6bb17de75d65c
placemeup_com <placemeup_com@s.paunin.com>
2013-10-03 19:23:26 +0100

    migrations in

---
+++ protected/migrations/m131003_181811_go.php
@@ -0,0 +1,25 @@
+<?php
+
+class m131003_181811_go extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m131003_181811_go does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
