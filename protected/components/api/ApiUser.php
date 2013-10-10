ea05c94ca8fce924fea68e921c5ef2464c0e04c9
Dmitriy Paunin <paunin@immo.ru>
2013-10-10 12:25:22 +0400

    Exceptions 403 API

--- protected/components/api/ApiUser.php
+++ protected/components/api/ApiUser.php
@@ -2,6 +2,6 @@
 
 class ApiUser extends WebUser{
     public function  loginRequired(){
-        throw new CHttpException(403);
+        throw new CHttpException(403,Yii::t('api','Token required/expired/incorrect'));
     }
 }
