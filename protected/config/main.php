7184c18f8b2d215dc3c2b5b66e78777f51fcb82e
paunin <info@paunin.com>
2013-11-22 22:40:06 +0400

    fixes

--- protected/config/main.php
+++ protected/config/main.php
@@ -4,7 +4,7 @@ $main = array(
 
     'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
     'name' => 'Yii Boilerplate App', //@ChangeIt
-    'language'=>'ru',
+    'language'=>'en',
     'sourceLanguage'=>'en',
 
     // preloading 'log' component
