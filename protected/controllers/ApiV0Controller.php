0fc28e771a007b3b50cbc86ea63b672a9472ff3e
paunin <info@paunin.com>
2013-12-09 19:45:34 +0400

    Merge remote-tracking branch 'placemeup/master' into master-github

--- protected/controllers/ApiV0Controller.php
+++ protected/controllers/ApiV0Controller.php
@@ -12,7 +12,21 @@ class ApiV0Controller extends ApiController
         return array_merge(
             parent::filters(),
             array(
-                array('COutputCache + index', 'duration' => 3600, 'varyByParam' => array('v')),
+                //array('COutputCache + index', 'duration' => 3600, 'varyByParam' => array('v')),
+            ),
+            array('apiAccessControl')
+        );
+    }
+
+
+
+
+    public function accessRules()
+    {
+        return array(
+            array('deny',
+                'actions' => array('index'),
+                'users' => array('?'),
             )
         );
     }
